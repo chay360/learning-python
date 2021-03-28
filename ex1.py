14477c60c5b34cca59f06b758f681764a9724c65
G KRISHNA CHAITANYA REDDY <38665671+chay360@users.noreply.github.com>
2021-03-28 10:45:51 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+print("Hello again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("study drill part 1")
+
+
+#hash symbol is to mention comments.
---
+++ ex5.py
@@ -0,0 +1,20 @@
+#More variables and printing
+
+
+my_name = 'krishna chaitanya'
+my_age = 0x15 #hex
+my_height = 2
+my_weight = 2.0
+my_eyes = 'eyes'
+my_teeth = "teeth"
+my_hair = "hair"
+
+print(f"Let's talk about {my_name}")
+print(f"He is {my_height} meters tall...??")
+print(f"He's {my_weight} tons heavy...!!")
+print("That's not good.")
+print(f"He's got {my_eyes}, {my_hair}, and most of his {my_teeth}")
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")
